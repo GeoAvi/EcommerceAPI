c50264bb7c480ac38a269f4fa851b243b4465b3d
GeoAvi <georgenavendalkaran@gmail.com>
2024-03-21 16:55:25 +0530

    My final commit

---
+++ config/mongoose.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+// connecting mongoose to its default server and ecommerceDB
+mongoose.connect(
+  'mongodb+srv://todoapp:todo-app@cluster0.hqe00m8.mongodb.net/EcommerceAPI?retryWrites=true&w=majority',
+  {
+    useNewUrlParser: true,
+  }
+);
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'Error connecting to MongoDB'));
+
+db.once('open', function () {
+  console.log('Connected to Database :: MongoDB');
+});
+
+module.exports = db;
